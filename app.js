eae420e3c6a24c3fc59ba0c1b4ced36f124c112d
Cindy Sun <c.s.sun5@gmail.com>
2016-08-22 16:34:24 -0700

    Search function working, get requests working

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 angular.module("hs", [])
 
-.controller("mainController", function($scope) {
-    $scope.title = 'Hello World!';
-});
+.config(function($httpProvider) {
+  $httpProvider.defaults.headers.get = {
+    'X-Mashape-Authorization': 'arTq6BLPQRmshAdHvIgnuloUhDTip1im2OqjsnQ0KISzmHOFJ2'
+  };
+})
 
-// .factory("CardDB", function() {
-//
-// });
+.controller("mainController", function($scope, CardDB) {
+  $scope.title = 'Hello World!';
+
+  $scope.cardInfo = {};
+
+  $scope.getCardInfo = function(name) {
+    CardDB.getCardsByName(name)
+      .then(function(data) {
+        $scope.cardInfo = data;
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+  };
+
+})
+
+.factory("CardDB", function($http) {
+
+
+  var getCardsByName = function(name) {
+    return $http({
+        method: 'GET',
+        url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards/search/' + name
+      })
+      .then(function(resp) {
+        return resp.data;
+      });
+  };
+
+  return {
+    getCardsByName: getCardsByName
+  };
+
+});
