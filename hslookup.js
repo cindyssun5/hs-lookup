526744b27fe8a906ecd757d50192cd09ceeae427
Cindy Sun <c.s.sun5@gmail.com>
2016-08-22 20:40:03 -0700

    Change capitalization of Port in hslookup.js, herokuapp deployed and working

--- hslookup.js
+++ hslookup.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.set('view engine', 'ejs');
-app.set('port', (process.env.port) || 4568)
+app.set('port', (process.env.PORT) || 4568);
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
